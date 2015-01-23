2d21d35f1b31f2d998cdd1020a8e2045b22c4aea
usr0300789 <usr0300789@APGA0007.local>
2015-01-23 11:01:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+MyApp4
+======
+
+A demo app with Apsalar SDK on Android ouput error log like "Are you missing a call to unregisterReceiver()?...".
+
+
+1. Before event tracking, You need to integerate Apsalar Android SDK.
+(import apsala.jar into app/libs/, etc...)
+
+ Refer to:
+ http://support.apsalar.com/customer/portal/articles/717507-integrating-the-android-sdk
+
+2. launch app
+3. tap send button
+4. back to main by up navigation on top left corner
+5. tap send button
+6. back to main by up navigation on top left corner
+7. confirm message "Are you missing a call to unregisterReceiver()?" in logcat.
+
+
---
+++ app/src/main/java/com/example/myapp4/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.myapp4;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.apsalar.sdk.Apsalar;
+
+public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.exapmle.myapp4.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(">>>>DEBUG<<<", "main activity onCreate called");
+        Apsalar.startSession(this, "<API_KEY>", "<SECRET>");
+        Apsalar.event("test_event");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Apsalar.unregisterApsalarReceiver();
+        Log.d(">>>>DEBUG<<<", "main activity onPause called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(">>>>DEBUG<<<", "main activity onDestroy called");
+    }
+
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
